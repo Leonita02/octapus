f3696a6a090e9d4142e2f3e4865d17c8f0c13f90
AltinaKasabaqi <altinakasabaqi@gmail.com>
2023-04-13 15:37:48 +0200

    SearchBar

---
+++ src/components/js/searchBar.js
@@ -0,0 +1,16 @@
+
+function search_Bar(){
+    return  <form action="/" method="get">
+    <input
+        type="text"
+        id=""
+        placeholder="Search..."
+        name="s" 
+    />
+    <button type="submit">Search</button>
+</form>
+    
+    
+}
+
+export default search_Bar;
