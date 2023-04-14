ea613ea280c530296c00219782edb16cb3b81d12
AltinaKasabaqi <altinakasabaqi@gmail.com>
2023-04-14 22:30:07 +0200

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import HomePage from "./components/js/HomePage";
 import './App.css';
 import Feed from './components/js/feed'
+import Dashboard from './components/js/dashboard';
 function App() {
-  return <Feed></Feed>;
+  return <Dashboard></Dashboard>
 }
 
 export default App;
