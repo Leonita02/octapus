03aa0e884d5fb9ff571ef8339d09c122c9b16d9f
Leonita02 <leonitaa.selmani@gmail.com>
2023-04-17 03:27:39 +0200

    setting the connection with database using NodeJS

---
+++ src/nodeJS/db_connection.js
@@ -0,0 +1,57 @@
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'octopus'
+//   });
+//   connection.connect((err) => {
+//     if (err) {
+//       console.error('Error connecting to database: ', err);
+//       return;
+//     }
+//     console.log('Connected to database.');
+//   });
+
+
+//   connection.query('SELECT * FROM your_table_name', (err, results) => {
+//     if (err) {
+//       console.error('Error executing query: ', err);
+//       return;
+//     }
+//     console.log('Query results: ', results);
+//   });
+
+
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'octopus',
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+
+  console.log('Connected to MySQL server.');
+
+  // The code below closes the connection to MySQL server
+
+  connection.end((err) => {
+    if (err) throw err;
+
+    console.log('Connection to MySQL server closed.');
+  });
+});
+
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM biblioteka", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
