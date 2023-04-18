ab13c35d12ac4673bb5f481d8bde5479e9c0a0b3
diellzaaa1 <diellzasalihu10@gmail.com>
2023-04-18 15:39:04 +0200

    some changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// import HomePage from "./components/js/HomePage";
-// import './App.css';
-// import Feed from './components/js/feed'
-// import Dashboard from './components/js/dashboard';
-// import ProfilePage from './components/js/profilePage';
+//import HomePage from "./components/js/HomePage";
+import './App.css';
+ //import Feed from './components/js/feed'
+ //import Dashboard from './components/js/dashboard';
+//import ProfilePage from './components/js/profilePage';
 import BookPage from "./components/js/bookPage";
 function App() {
   return <BookPage></BookPage>
