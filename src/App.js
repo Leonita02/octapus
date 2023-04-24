3c3f32476795bbf600609351fb474d4e8bda64ab
AltinaKasabaqi <altinakasabaqi@gmail.com>
2023-04-24 21:57:38 +0200

    #

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 // import './App.css';
 // import Feed from './components/js/feed'
 // import Dashboard from './components/js/dashboard';
-// import ProfilePage from './components/js/profilePage';
+import ProfilePage from './components/js/profilePage';
 import BookPage from "./components/js/bookPage";
 
 function App() {
-  return <HomePage></HomePage>
+  return <ProfilePage></ProfilePage>
     
 }
 
