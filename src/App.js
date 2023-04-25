298d9ec6530a521897454d2e689b302e1c518c77
Leonita02 <leonitaa.selmani@gmail.com>
2023-04-25 23:17:42 +0200

    made a correction in app.js

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import HomePage from "./components/js/HomePage";
+import './App.css';
+ //import Feed from './components/js/feed'
+ //import Dashboard from './components/js/dashboard';
+//import ProfilePage from './components/js/profilePage';
+ import HomePage from "./components/js/HomePage";
+// import './App.css';
+// import Feed from './components/js/feed'
+// import Dashboard from './components/js/dashboard';
+// import ProfilePage from './components/js/profilePage';
+import BookPage from "./components/js/bookPage";
+import WishList from './components/js/wishList';
+
+function App() {
+  return <WishList></WishList>
+ 
+}
+
+export default App;
