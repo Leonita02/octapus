30e0eff36574b4a45079188df5b097dd8ce1371b
diellzaaa1 <diellzasalihu10@gmail.com>
2023-04-28 14:41:19 +0200

    modifikime ne addBook page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
  //import Feed from './components/js/feed'
  //import Dashboard from './components/js/dashboard';
 //import ProfilePage from './components/js/profilePage';
- import HomePage from "./components/js/HomePage";
+// import HomePage from "./components/js/HomePage";
 // import './App.css';
 // import Feed from './components/js/feed'
 // import Dashboard from './components/js/dashboard';
 // import ProfilePage from './components/js/profilePage';
-import BookPage from "./components/js/bookPage";
-import WishList from './components/js/wishList';
-import Porosia from './components/js/porosia';
-import Pagesa from './components/js/pagesa';
+//import BookPage from "./components/js/bookPage";
+//import WishList from './components/js/wishList';
+//import Porosia from './components/js/porosia';
+//import Pagesa from './components/js/pagesa';
+import AddBook from './components/js/addBook';
+// import VForm from './components/js/VForm'
 function App() {
-  return <Pagesa/>
+  return <AddBook/>
  
 }
 
