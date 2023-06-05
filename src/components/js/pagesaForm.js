9dbd42547a4717b2ba1fd6ad00e212445a05c69e
Leonita02 <leonitaa.selmani@gmail.com>
2023-06-05 23:12:23 +0200

    Implemented the cash payment for the employee

--- src/components/js/pagesaForm.js
+++ src/components/js/pagesaForm.js
@@ -6,12 +6,8 @@ export default function PagesaForm() {
 
     const navigate = useNavigate();
     const [values, setValues] = useState({
-        Personi_ID: '',
         qyteti: '',
         email: '',
-        emriKarteles: '',
-        nrKarteles: '',
-        data_pageses: ''
     });
 
     const [errors, setErrors] = useState({});
@@ -26,22 +22,12 @@ export default function PagesaForm() {
     const handleInput = (event) => {
         const { name, value } = event.target;
         let error = null;
+
         // Regex validation
-        const regexPersoni_ID = /^[A-Za-z0-9]{1,}$/;
         const regexQyteti = /^[A-Za-z]+$/;
         const regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
-        const regexEmriKarteles = /^[A-Za-z]+$/;
-        const regexNrKarteles = /^[0-9]{5,}$/;
-        const regexDataPageses = /^\d{4}-\d{2}-\d{2}$/;
 
         switch (name) {
-            case 'Personi_ID':
-                if (!value.trim()) {
-                    error = 'Personi ID nuk duhet te jete e zbrazet!';
-                } else if (!regexPersoni_ID.test(value)) {
-                    error = 'Personi ID e dhene ne forme te gabuar!';
-                }
-                break;
             case 'email':
                 if (!value.trim()) {
                     error = 'Email nuk duhet te jete e zbrazet!'
@@ -50,28 +36,12 @@ export default function PagesaForm() {
                     error = 'Emaili jo valid!';
                 }
                 break;
-            case 'emriKarteles':
+            case 'qyteti':
                 if (!value.trim()) {
-                    error = 'Emri i Karteles nuk duhet te jete e zbrazet!'
+                    error = 'Qyteti nuk duhet te jete zbrazet!'
                 }
-                else if (!regexEmriKarteles.test(value)) {
-                    error = 'Emri i Karteles jo valid!';
-                }
-                break;
-            case 'nrKarteles':
-                if (!value.trim()) {
-                    error = 'Numri i Karteles nuk duhet te jete e zbrazet!'
-                }
-                else if (!regexNrKarteles.test(value)) {
-                    error = 'Numri i Karteles jo valid!';
-                }
-                break;
-            case 'data_pageses':
-                if (!value.trim()) {
-                    error = 'Data e pageses nuk duhet te jete e zbrazet!'
-                }
-                else if (!regexDataPageses.test(value)) {
-                    error = 'Data e Pageses jo valide!';
+                else if (!regexQyteti.test(value)) {
+                    error = 'Vlera e qytetit jo valide!';
                 }
                 break;
             default:
@@ -82,44 +52,34 @@ export default function PagesaForm() {
         setErrors((prev) => ({ ...prev, [name]: error }));
     };
 
+
     const handleSubmitP = (event) => {
+
         event.preventDefault();
+
+        if (values.email == '' || values.qyteti == '') {
+            alert('Please fill the required fields!');
+            return;
+        }
+
         // Checking if there are any errors for the validation with regex
         const hasErrors = Object.values(errors).some((error) => error !== null);
 
         if (hasErrors) {
             console.log('Form contains errors:', errors);
-            return;
+            return; 
         }
-        //Test if the person is in the database
-        axios
-            .get('http://localhost:8081/pagesaRepo/', {
-                params: {
-                    Personi_ID: values.Personi_ID,
-                },
-            })
-            .then((res) => {
-                const { exists } = res.data;
 
-                if (exists) {
-                    // Personi_ID exists in the database, proceed with form submission
-                    axios
-                        .post('http://localhost:8081/pagesaRepo/', values)
-                        .then((res) => {
-                            const responseMessage = res.data.message;
-                            console.log('Response:', responseMessage);
-                            alert(successMessage);
-                            setSuccessMessage(responseMessage);
-                        })
-                        .catch((err) => console.log(err));
-                } else {
-                    // Personi_ID does not exist in the database, display an error message or take appropriate action
-                    alert('Personi_ID does not exist in the database');
-                }
+        axios.post(`http://localhost:8081/pagesat/personat/${values.email}`, {
+            qyteti: values.qyteti
+        })
+            .then((res) => {
+                alert(res.data.message);
+                console.log({ res })
             })
-            .catch((error) => {
-                console.log('Error:', error);
-            });
+            .catch((err) => {
+                console.error(err)
+            })
     };
 
     return (
@@ -130,32 +90,6 @@ export default function PagesaForm() {
                         <div className="card-body">
                             <h2 className="card-title text-center mb-4">Paguaj Kesh</h2>
                             <form className="needs-validation" noValidate>
-                                <div className="form-group">
-                                    <label htmlFor="Personi_ID">Personi ID</label>
-                                    <input
-                                        type="text"
-                                        className={`form-control ${errors.Personi_ID ? 'is-invalid' : ''}`}
-                                        id="Personi_ID"
-                                        name="Personi_ID"
-                                        value={values.Personi_ID}
-                                        onChange={handleInput}
-                                        required
-                                    />
-                                    {errors.Personi_ID && <div className="invalid-feedback">{errors.Personi_ID}</div>}
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="qyteti">Qyteti</label>
-                                    <input
-                                        type="text"
-                                        className={`form-control ${errors.qyteti ? 'is-invalid' : ''}`}
-                                        id="qyteti"
-                                        name="qyteti"
-                                        value={values.qyteti}
-                                        onChange={handleInput}
-                                        required
-                                    />
-                                    {errors.qyteti && <div className="invalid-feedback">{errors.qyteti}</div>}
-                                </div>
                                 <div className="form-group">
                                     <label htmlFor="email">Email</label>
                                     <input
@@ -170,44 +104,17 @@ export default function PagesaForm() {
                                     {errors.email && <div className="invalid-feedback">{errors.email}</div>}
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="emriKarteles">Emri i Karteles</label>
+                                    <label htmlFor="qyteti">Qyteti</label>
                                     <input
                                         type="text"
-                                        className={`form-control ${errors.emriKarteles ? 'is-invalid' : ''}`}
-                                        id="emriKarteles"
-                                        name="emriKarteles"
-                                        value={values.emriKarteles}
+                                        className={`form-control ${errors.qyteti ? 'is-invalid' : ''}`}
+                                        id="qyteti"
+                                        name="qyteti"
+                                        value={values.qyteti}
                                         onChange={handleInput}
                                         required
                                     />
-                                    {errors.emriKarteles && <div className="invalid-feedback">{errors.emriKarteles}</div>}
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="nrKarteles">Numri i Karteles</label>
-                                    <input
-                                        type="text"
-                                        className={`form-control ${errors.nrKarteles ? 'is-invalid' : ''}`}
-                                        id="nrKarteles"
-                                        name="nrKarteles"
-                                        value={values.nrKarteles}
-                                        onChange={handleInput}
-                                        required
-                                    />
-                                    {errors.nrKarteles && <div className="invalid-feedback">{errors.nrKarteles}</div>}
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="data_pageses">Data e Pageses</label>
-                                    <input
-                                        placeholder='yyyy-mm-dd'
-                                        type="text"
-                                        className={`form-control ${errors.data_pageses ? 'is-invalid' : ''}`}
-                                        id="data_pageses"
-                                        name="data_pageses"
-                                        value={values.data_pageses}
-                                        onChange={handleInput}
-                                        required
-                                    />
-                                    {errors.data_pageses && <div className="invalid-feedback">{errors.data_pageses}</div>}
+                                    {errors.qyteti && <div className="invalid-feedback">{errors.qyteti}</div>}
                                 </div>
                                 <br />
                                 <button className="btn btn-primary btn-lg btn-block custom-button" type="submit" onClick={handleSubmitP}>
--- src/nodeJS/pagesaRepo.js
+++ src/nodeJS/pagesaRepo.js
@@ -1,46 +1,27 @@
-const express=require('express');
-const router=express.Router();
-const connection=require('./db_connection');
+const express = require('express');
+const router = express.Router();
+const connection = require('./db_connection');
 
-//Marrja e te dhenave nga databaza per testim te Personi_ID
-router.get('/', async (req, res) => {
-    const { Personi_ID } = req.query;
-  
-    // Perform a database query to check if the Personi_ID exists in the personi table
-    const personi = await Personi.findOne({
-      where: {
-        Personi_ID: Personi_ID,
-      },
-    });
-    if (personi) {
-        res.json({ exists: true });
-      } else {
-        res.json({ exists: false });
-      }
+
+router.post("/personat/:email", (req, res) => {
+
+  console.log("erdh te endpointi i pagesave!", { params: req.params, body: req.body });
+
+  const { email: p_email } = req.params;
+  const { qyteti } = req.body;
+
+  console.log({ email: p_email, qyteti });
+
+  const sqlProcedureCommand = `CALL make_cash_payment_for_user(?, ?)`;
+
+  connection.query(sqlProcedureCommand, [p_email, qyteti], (err, results) => {
+    if (err) {
+      console.log('Error:', err); // log the error, if any
+      return res.json({ succeeded: false, message: err });
+    }
+    console.log('results:', results); // log the query results
+    return res.json({ succeeded: true, message: "Payment completed successfully!" });
+  });
 });
 
-//Ruajtja e te dhenave ne databaze
-router.post("/", (req, res) => {
-    const { Personi_ID,qyteti,email,emriKarteles,nrKarteles,data_pageses} = req.body;
-  
-    const sqlInsertCommand = `INSERT INTO pagesa (Personi_ID, qyteti, email, emriKarteles, nrKarteles, data_pageses) VALUES (?, ?, ?, ?, ?, ?)`;
-   
-  
-    connection.query(
-      sqlInsertCommand,
-      [Personi_ID, qyteti, email, emriKarteles, nrKarteles, data_pageses],
-      (err, results) => {
-        if (err) {
-          console.log('Error:', err);
-          return res.json("Error");
-        }
-        console.log('results:', results);
-        const successMessage = "Pagesa u krye me sukses";
-        // return res.json(results);
-        return res.json({ message: successMessage });
-        
-      }
-    );
-  });
-
-  module.exports = router;
+module.exports = router;
