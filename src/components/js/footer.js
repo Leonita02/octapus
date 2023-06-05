aa964351024fa72572e3423d5d5d31eeb015503d
diellzaaa1 <diellzasalihu10@gmail.com>
2023-06-05 16:46:34 +0200

    Insert/fetch-WishCard from the current User

--- src/components/js/footer.js
+++ src/components/js/footer.js
@@ -1,31 +1,47 @@
-import React from "react";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import logo from '../ImagesOfProject/instagram_profile_image.png';
 import '../css/footer.css';
-import fbLogo from '../ImagesOfProject/facebook.png';
-import instaLogo from '../ImagesOfProject/instagram.png';
-import twitterLogo from '../ImagesOfProject/twitter.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
-function footer(){
-    return(
-
-        <footer>
-        <div className="footer">
-          <div className="footer-logo">
-            <img src={logo} alt="logo"/>
-          </div>
-          <div className="socialLogo">
-            <ul>
-              <li><img src={fbLogo} alt="Facebook"/></li>
-              <li><img src={instaLogo} alt="Instagram"/></li>
-              <li><img src={twitterLogo} alt="Twitter"/></li>
-            </ul>
-          </div>
-        </div>
-      </footer>
 
 
-    );
-}
+const Footer = () => {
+  return (
+    <footer className="bg-footer text-white">
+      <Container className="py-4">
+        <Row className="align-items-center">
+          <Col className="text-left">
+            <img src={logo} alt="Your Logo" className="footer-logo mb-3" />
+          </Col>
+          <Col className="text-right">
+            <a href="#" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FontAwesomeIcon icon={faFacebook} className="mr-4 icon-lg" />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FontAwesomeIcon icon={faTwitter} className="mr-4 icon-lg" />
+            </a>
+            <a href="#" target="_blank" rel="noopener noreferrer" className="social-link">
+              <FontAwesomeIcon icon={faInstagram} className="mr-4 icon-lg" />
+            </a>
+            <a href="mailto:your-email@example.com" className="social-link">
+              <FontAwesomeIcon icon={faEnvelope} className="icon-lg" />
+            </a>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col className="text-center">
+            <p className="spicy-text">© 2023 Your Company. All rights reserved.</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default footer;
+export default Footer;
--- src/components/js/wishList.js
+++ src/components/js/wishList.js
@@ -2,27 +2,36 @@ import React from 'react';
 import Nav from './nav';
 import Footer from '../js/footer';
 import image from '../ImagesOfProject/foto1.avif';
-import wishList from '../css/wishList.css';
+ import  '../css/wishList.css';
 import { useNavigate, useParams } from 'react-router';
 import { useState } from 'react';
+
+import {useCookies} from 'react-cookie';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBook } from '@fortawesome/free-solid-svg-icons';
 
 export default function WishL() {
     const [titulli, setTitulli] = useState("")
     const [autori, setAutori] = useState("")
+    const [cookies] = useCookies(['userId', 'roleId']);
+
+    // Access the user ID and role ID from cookies
+    const userId = cookies.userId;
+    const roleId = cookies.roleId;
     
     const navigate = useNavigate();
 
     function handleSubmit(event) {
-    
-
         event.preventDefault();
-        axios.post("http://localhost:8081/wishList", { titulli, autori})
-            .then(res => {
-                console.log(res);
-                 navigate('/profilePage'); // ose ne feed qetu duhet me bo kushtin me kshyr rolin kur te regjistrojme
-            }).catch(err => console.log(err));
-    }
+        console.log("userId from cookie:", userId);
+        axios.post("http://localhost:8081/wishList", { titulli, autori, userId })
+          .then(res => {
+            console.log(res);
+            navigate('/profilePage');
+          })
+          .catch(err => console.error(err));
+      }
 
     
 
@@ -32,28 +41,81 @@ export default function WishL() {
             <div><Nav /></div>
             <br />
             <br />
+            <br></br>
+            <br></br>
+            <br></br>
+            <div className="container custom-container">
+  <div className="row">
+    <div className="col-md-6 offset-md-3">
+      <div className="card border-0 shadow" style={{ minHeight: "450px" }}>
+        <div className="card-header bg-custom text-white text-center" style={{ backgroundColor: " RGB(130, 179, 229)"}}>
+          <h1>Wish-Card</h1>
+        </div>
+        <br></br>
+       
+        <div className="card-body">
             
-            <div className='first'>
-                <div id="wishBanner">
-                    <center>
-                         <h1>Lista juaj e deshirave:</h1>
-                    </center>
+          <form onSubmit={handleSubmit} id="form">
+            <div className="row">
+              <div className="col">
+                <div className="form-group">
+                  <label htmlFor="titulli" style={{ fontWeight: "bold", fontSize: "18px" }}>Titulli:</label>
+                  <br></br>
+            
+                  <input
+                    type="text"
+                    className="form-control custom-input"
+                    id="titulli"
+                    placeholder="Shkruani titullin"
+                    style={{ marginLeft: "1px" ,border: "3px solid #ccc" }}
+                    // style={{ width: "250px" }}
+                    onChange={(e) => setTitulli(e.target.value)}
+                  />
                 </div>
-                <div className="labels">
-                    <form onSubmit = { handleSubmit } id='form'>
-                        <input type="text" className="form-control" placeholder='Titulli:' id='titulli' onChange={e => setTitulli(e.target.value)} />
-                        <input type="text" className="form-control" placeholder='Autori:' id='autori' onChange={e => setAutori(e.target.value)} />
-                
-                        <br></br>
-                        <button id="vazhdo" type='submit'>Shto Librin!</button>
-                       
-                    </form>
-                </div>
+              </div>
             </div>
+            <br></br>
+            
+            <div className="row">
+              <div className="col">
+                <div className="form-group">
+                  <label htmlFor="autori" style={{ fontWeight: "bold", fontSize: "18px" }}>Autori:</label>
+                  <input
+                    type="text"
+                    className="form-control custom-input"
+                    id="autori"
+                    style={{border: "3px solid #ccc" }}
+                    placeholder="Shkruani autorin"
+                    onChange={(e) => setAutori(e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
+            <br>
+            </br>
+          
+            <div className="row">
+              <div className="col">
+              <button
+            type="submit"
+  className="btn btn-custom btn-block ml-auto"
+  style={{ backgroundColor: "RGB(130, 179, 229)", color: "white", width: "120%" }}
+>
+  Shto në Wish-List
+</button>
+              </div>
+            </div>
+          
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
             <br />
             <br />
             <br />
-            <div><Footer /></div>
+            {/* <div><Footer /></div> */}
         </>
     );
 }
